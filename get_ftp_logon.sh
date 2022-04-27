f21af96045a6ee37bab0b1f2b1813de7e416716e
condor0010 <condor0010@laptop>
2022-04-27 14:17:21 -0400

    last min fix

--- get_ftp_logon.sh
+++ get_ftp_logon.sh
@@ -2,11 +2,12 @@
 for arg in "$@"; do
   input=$(tshark -r $arg -Y ftp)
   for i in $(echo "$input" | grep USER | awk '{print $10}'); do
-    if [ -z $(echo "$input" | grep -A 3 $i | grep "cannot" | awk '{print $9}') ]; then
+    if [ -Z $(echo "$input" | grep -A 3 $i | grep "cannot" | awk '{print $9}') ]; then
       echo "sucessfull logon attempt"
     else
-      echo "failed logon attempt"
+      echo "sfailed logon attempt"
     fi
     echo "$input" | grep -A 3 $i | grep "USER\|PASS" | awk '{print "    "$9" "$10}'
   done
 done
+
